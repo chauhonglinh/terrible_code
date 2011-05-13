cd4afbacebdb775a952b8db24825b81c3ede452a
Chau Hong Linh <chauhonglinh@hanoian.com>
2011-05-12 21:27:46 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TerribleCode::Application.config.secret_token = '91c4027b4e70ffaf66daacc4be5c09099eecddbf0d049dda1291cd7fbf2263052b88f08608bcd7eaeefb38e80a44bfaec9a75c9a69d980abfbc0ba697e5a29d8'
